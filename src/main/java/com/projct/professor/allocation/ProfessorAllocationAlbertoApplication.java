d3ebb78fd914b219066efcb7b1559d7756a2966b
José Alberto Claudino Silva <pr.josealbertoibc@gmail.com>
2024-02-24 14:29:30 -0300

    add project

---
+++ src/main/java/com/projct/professor/allocation/ProfessorAllocationAlbertoApplication.java
@@ -0,0 +1,13 @@
+package com.projct.professor.allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationAlbertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationAlbertoApplication.class, args);
+	}
+
+}
